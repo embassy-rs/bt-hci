3dc1fd0f7de6e13a0a308200ff7c48a07ab7d392
Alex Moon <alex@moonspot.org>
2024-04-19 18:19:31 -0400

    Add CI workflow

---
+++ ci.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+cargo +nightly fmt -- --check
+
+cargo clippy
+cargo clippy --features embassy-time
+
+cargo clippy --features defmt
+cargo clippy --features defmt,embassy-time
+
+cargo clippy --features log
+cargo clippy --features log,embassy-time
+
+cargo test --features embassy-time
