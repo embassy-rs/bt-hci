720d96d3b66dd0773d954b519ecb41a7b6b24402
James Sizeland <james.sizeland@dyson.com>
2024-10-31 16:32:56 +0000

    add appearance generation code

--- update_uuids/src/main.rs
+++ update_uuids/src/main.rs
@@ -10,23 +10,48 @@ mod writer;
 mod yaml;
 
 use git2::Repository;
-use std::{error::Error, path::Path};
+use std::{
+    error::Error,
+    path::{Path, PathBuf},
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Download the latest assigned numbers from the Bluetooth SIG
     const SIG_URL: &str = "https://bitbucket.org/bluetooth-SIG/public.git";
     println!("Downloading the latest assigned numbers from the Bluetooth SIG...");
 
-    let local_path = Path::new("bluetooth-sig");
-    fetch_repo(SIG_URL, local_path)?;
-
-    // Load the YAML data from ./bluetooth-sig/assigned_numbers/uuids*
-    let path = local_path.join("assigned_numbers").join("uuids");
-    let map = yaml::load_uuid_data(&path)?;
-    println!("{:?}", map);
+    let local_folder = Path::new("bluetooth-sig");
     let output_folder = Path::new("../src/uuid");
+    fetch_repo(SIG_URL, local_folder)?;
+
+    write_uuids(local_folder, output_folder)?; // assigned_numbers/uuids
+
+    write_appearance(local_folder, output_folder)?; // assigned_numbers/core/appearance
+
+    Ok(())
+}
+
+/// Parse and write the UUIDs to the source code
+/// The UUIDs are loaded from the YAML files in the assigned_numbers/uuids folder
+fn write_uuids(local_folder: &Path, output_folder: &Path) -> Result<(), Box<dyn Error>> {
+    // Load the YAML data from ./bluetooth-sig/assigned_numbers/uuids*
+    let path = local_folder.join("assigned_numbers").join("uuids");
+    let uuid_map = yaml::load_uuid_data(&path)?;
     // Update the assigned numbers in the source code
-    writer::update_uuids(output_folder, &map)?;
+    writer::update_uuids(output_folder, uuid_map)?;
+    Ok(())
+}
+
+/// Parse and write the Appearance values to the source code
+/// The Appearance values are loaded from the YAML files in the assigned_numbers/core/appearance folder
+fn write_appearance(local_folder: &Path, output_folder: &Path) -> Result<(), Box<dyn Error>> {
+    // Load the YAML data from ./bluetooth-sig/assigned_numbers/core/appearance_values.yaml
+    let file_name = "appearance_values.yaml";
+    let path = local_folder.join("assigned_numbers").join("core").join(file_name);
+    let appearance_data = yaml::load_appearance_data(&path)?;
+    println!("{:?}", appearance_data);
+    // Update the appearance values in the source code
+    writer::update_appearance(output_folder, &appearance_data)?;
     Ok(())
 }
 
