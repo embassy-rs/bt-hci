1ff42eb7f5f01661534435f10d1a34ac332b3f7e
James Sizeland <james.sizeland@dyson.com>
2024-10-31 16:32:23 +0000

    cover more edgecases on name

--- update_uuids/src/utils.rs
+++ update_uuids/src/utils.rs
@@ -19,21 +19,40 @@ pub fn screaming_snake_case(s: &str) -> String {
             result.push(first.to_ascii_uppercase());
         }
     }
-    for c in chars {
+    let mut prev: Option<char> = None;
+    for (index, c) in chars.enumerate() {
         if c.is_ascii_uppercase() {
-            result.push('_'); // insert underscore before uppercase letter, except the first letter
-            result.push(c.to_ascii_uppercase());
+            // handle existing uppercase characters that might be part of an acronym
+            // as well as PascalCase
+            // if previous character was not uppercase, insert underscore
+            if let Some(prev) = prev {
+                if !prev.is_ascii_uppercase() {
+                    result.push('_');
+                } else if let Some(next) = s.chars().nth(index + 2) {
+                    // if next character is lowercase, insert underscore
+                    if next.is_ascii_lowercase() {
+                        result.push('_');
+                    }
+                }
+            }
         } else if c.is_whitespace() {
             result.push('_'); // replace whitespace with underscore
         } else if c == '-' {
             result.push('_'); // replace hyphen with underscore
         } else if c == '_' {
             result.push('_'); // keep existing underscore
-        } else if c.is_alphanumeric() {
+        }
+        if c.is_alphanumeric() {
             result.push(c.to_ascii_uppercase());
         } // ignore other characters
+        prev = Some(c);
     }
-    result = result.replace("TEXTSUBSCRIPT", "").replace("___", "_"); // special case
+    result = result // special cases
+        .replace("TEXTSUBSCRIPT", "")
+        .replace("___", "_")
+        .replace("__", "_")
+        .replace('å', "A")
+        .replace('ö', "O");
     result
 }
 
@@ -48,18 +67,11 @@ mod test {
         assert_eq!(screaming_snake_case("DST Offset"), "DST_OFFSET");
         assert_eq!(screaming_snake_case("Headset - HS"), "HEADSET_HS");
         assert_eq!(screaming_snake_case("GAP"), "GAP");
-        assert_eq!(
-            screaming_snake_case("pressure (pound-force per square inch)"),
-            "PRESSURE_POUND_FORCE_PER_SQUARE_INCH"
-        );
-        assert_eq!(
-            screaming_snake_case("CIE 13.3-1995 Color Rendering Index"),
-            "CIE_133_1995_COLOR_RENDERING_INDEX"
-        );
-        assert_eq!(
-            screaming_snake_case("CO\\\\textsubscript{2} Concentration"),
-            "CO2_CONCENTRATION"
-        );
+        assert_eq!(screaming_snake_case("length (ångström)"), "LENGTH_ANGSTROM");
+        assert_eq!(screaming_snake_case("LANAccessUsingPPP"), "LAN_ACCESS_USING_PPP");
+        assert_eq!(screaming_snake_case("pressure (pound-force)"), "PRESSURE_POUND_FORCE");
+        assert_eq!(screaming_snake_case("CIE 13.3-1995 Color"), "CIE_133_1995_COLOR");
+        assert_eq!(screaming_snake_case("CO\\\\textsubscript{2} Conc"), "CO2_CONC");
         assert_eq!(screaming_snake_case("3D Display"), "_3D_DISPLAY");
     }
 }
