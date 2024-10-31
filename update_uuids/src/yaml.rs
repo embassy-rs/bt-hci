c9efe686e2b72e6fa75c6d61f623f3caf2d27199
James Sizeland <james.sizeland@dyson.com>
2024-10-31 16:32:56 +0000

    add appearance generation code

--- update_uuids/src/yaml.rs
+++ update_uuids/src/yaml.rs
@@ -20,6 +20,8 @@ struct Uuids {
     uuids: Vec<UuidData>,
 }
 
+/// Load UUID data from a directory of YAML files, 
+/// matches files in the bluetooth-sig/assigned_numbers/uuids folder.
 pub fn load_uuid_data(path: &PathBuf) -> Result<HashMap<String, Vec<UuidData>>, Box<dyn Error>> {
     let mut map = HashMap::new();
     for entry in WalkDir::new(path) {
@@ -33,6 +35,35 @@ pub fn load_uuid_data(path: &PathBuf) -> Result<HashMap<String, Vec<UuidData>>, 
         };
     }
     Ok(map)
+}
+
+#[derive(Debug, Deserialize)]
+struct AppearanceValues {
+    appearance_values: Vec<Category>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Category {
+    pub category: u8,
+    pub name: String,
+    pub subcategory: Option<Vec<Subcategory>>
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Subcategory {
+    pub value: u8,
+    pub name: String,
+}
+
+/// Load UUID data from the appearance folder
+/// This has a different structure than the UUIDs folder
+pub fn load_appearance_data(path: &PathBuf) -> Result<Vec<Category>, Box<dyn Error>> {
+    if path.file_name() != Some("appearance_values.yaml".as_ref()) {
+        return Err("Invalid file name, must be appearance_values.yaml".into());
+    }
+    let data = std::fs::read_to_string(path)?;
+    let parsed_data: AppearanceValues = serde_yaml::from_str(&data)?;
+    Ok(parsed_data.appearance_values)
 }
 
 fn get_file_name(path: &Path) -> Option<String> {
